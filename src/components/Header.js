3ecb638f4d0084c12a608dab133169db478dd6a5
Zac Arellano <zaccc@email.arizona.edu>
2017-04-28 12:07:35 -0700

    project set-up

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => (
+  <header>
+    <h1>{'Care Revolutions | loading screen | tic-tac-toe'}</h1>
+  </header>
+)
+
+export default Header
---
+++ src/containers/AppContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import App from '../components/App'
+
+class AppContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+  componentDidMount() {
+
+  }
+  render() {
+    return <App />
+  }
+}
+
+export default AppContainer
